f1fc3a16aa2e18abdfde1593d745fd1e57709338
leegaarden <dlwjddnjs081723@naver.com>
2025-03-29 02:46:20 +0900

    init

---
+++ src/main/java/com/timeto/TimetoApplication.java
@@ -0,0 +1,13 @@
+package com.timeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimetoApplication.class, args);
+    }
+
+}
